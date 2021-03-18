1e66fc24b7c49f3df169c3ddefa5497f25217d7d
Dinesh Ponnaganti <Dinesh@zilliontechnologies.com>
2021-03-18 15:31:46 +0530

    application

---
+++ BuildDashboard/Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+﻿using BuildDashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BuildDashboard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            BuildDashboard.Models.DashboardContent dc;
+            dc = new BuildDashboard.Models.DashboardContent();
+            //dc.ResponseData = "Data received from the server";
+
+            using (var client = new HttpClient())
+            {
+                //var url = "https://ci.jenkins.io/api/";
+                //var url = "http://localhost:8080/job/DotNet/3/consoleText";
+                var url = "http://localhost:8080/job/DotNet/api/json?tree=builds[number,status,timestamp,id,result]";
+                
+                var response = client.GetAsync(url).Result;
+                BuildDetails bd = new BuildDetails();
+                Build bui = new Build();
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var responseContent = response.Content;
+
+                    string responseString = responseContent.ReadAsStringAsync().Result;
+                    
+
+                    //dc.ResponseData = responseString;
+
+                    bd = JsonConvert.DeserializeObject<BuildDetails>(responseString);
+                    //bui = JsonConvert.DeserializeObject<Build>(responseString);
+
+
+                }
+
+                return View(bd);
+
+            }
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/* using (var client = new HttpClient())
+ {
+     client.BaseAddress = new Uri("https://ci.jenkins.io/api/");
+     var responseTask = client.GetAsync("text");
+     responseTask.Wait();
+     var result = responseTask.Result;
+     if (result.IsSuccessStatusCode)
+     {
+         var readjob = result.Content.ReadAsStringAsync<IList<ErrorViewModel>>();
+         readjob.Wait();
+         IEnumerable<ErrorViewModel> text = readjob.Result;
+     }
+ }*/
+//using (var httpClient = new HttpClient())
+//{
+//    using (var response = await _client.GetAsync("localhost:8080/job/DotNet/3/consoleText"))
+//    {
+//        var apiResponse = await response.Content.ReadAsStringAsync();
+//        var task = JsonConvert.DeserializeObject(apiResponse);
+
+//        return task;
+//    }
+//}
---
+++ BuildDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace BuildDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+    public class DashboardContent
+    {
+        public string ResponseData { get; set; }
+    }
+
+    
+    public class BuildDetails
+    {
+        public string _class { get; set; }
+        public List<Build> builds { get; set; }
+    }
+    
+    public class Build
+    {
+        public string _class { get; set; }
+        public string id { get; set; }
+
+        public int number { get; set; }
+
+        public string result { get; set; }
+
+        public long timestamp { get; set; }
+    }
+}
